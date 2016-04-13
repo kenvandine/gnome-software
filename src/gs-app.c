6a4f75311ec4a871c6ce2b8aea758e771569eced
William Hua <william.hua@canonical.com>
2016-04-13 10:23:32 -0400

    Allow transition from AS_APP_STATE_INSTALLED to AS_APP_STATE_UPDATABLE

--- src/gs-app.c
+++ src/gs-app.c
@@ -498,6 +498,8 @@ gs_app_set_state_internal (GsApp *app, AsAppState state)
 	case AS_APP_STATE_INSTALLED:
 		/* installed has to go into an action state */
 		if (state == AS_APP_STATE_UNKNOWN ||
+		    state == AS_APP_STATE_UPDATABLE ||
+		    state == AS_APP_STATE_UPDATABLE_LIVE ||
 		    state == AS_APP_STATE_REMOVING)
 			state_change_ok = TRUE;
 		break;
