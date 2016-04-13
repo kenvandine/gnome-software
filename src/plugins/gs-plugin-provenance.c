39f8bbccdcd314bab9f531233570f26dde3f811c
Iain Lane <iain@orangesquash.org.uk>
2016-04-13 18:43:08 +0100

    provenance: Order after apt so we have the origin set
    ubuntu-specific: apt should use order_before, but this seems buggy and
    creates a loop currently.

--- src/plugins/gs-plugin-provenance.c
+++ src/plugins/gs-plugin-provenance.c
@@ -94,6 +94,7 @@ const gchar **
 gs_plugin_order_after (GsPlugin *plugin)
 {
 	static const gchar *deps[] = {
+		"apt",
 		"packagekit-refine",	/* after the package source is set */
 		NULL };
 	return deps;
@@ -157,8 +158,10 @@ gs_plugin_refine_app (GsPlugin *plugin,
 
 	/* simple case */
 	origin = gs_app_get_origin (app);
+	g_debug ("prov: considering %s", gs_app_get_id (app));
 	if (origin != NULL && gs_utils_strv_fnmatch (sources, origin)) {
 		gs_app_add_quirk (app, AS_APP_QUIRK_PROVENANCE);
+		g_debug ("prov: %s", gs_app_to_string (app));
 		return TRUE;
 	}
 
